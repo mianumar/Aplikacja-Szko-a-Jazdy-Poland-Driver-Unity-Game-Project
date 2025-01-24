d935dd2ced116f383e45c280cfec848049b5c56f
Muhammad Umar <50618335+mianumar@users.noreply.github.com>
2025-01-24 14:13:21 +0500

    Revert "Resolved conflicts by keeping my changes"
    This reverts commit f36c52573fc3a8b76e418b60b7321738cfccbfda, reversing
    changes made to eaf66e5277861939a4e078ab186f99904231a1f2.

--- Assets/Scripts/Database/DatabaseHandler.cs
+++ Assets/Scripts/Database/DatabaseHandler.cs
@@ -1,9 +1,7 @@
 using Firebase;
-using Firebase.Auth;
 using Firebase.Database;
 using Firebase.Extensions;
 using UnityEngine;
-
 
 public class DatabaseHandler : MonoBehaviour
 {
@@ -12,9 +10,7 @@ public class DatabaseHandler : MonoBehaviour
     public static DatabaseHandler Instance { get { return instance; } }
 
     private DatabaseReference dbRef;
-    private FirebaseAuth auth;
-
-    public FirebaseAuth Auth => auth;
+    private FirebaseApp firebaseAppRef;
 
     private void Awake()
     {
@@ -22,49 +18,35 @@ public class DatabaseHandler : MonoBehaviour
         {
             instance = this; 
         }
-
-        FirebaseApp.Create();
-        InitializeFirebase();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       // InitializeFirebase();
     }
 
-    /// <summary>
-    /// Initialize firebase 
-    /// </summary>
     private async void InitializeFirebase()
     {
         await FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
             var dependencyStatus = task.Result;
-            if (dependencyStatus == DependencyStatus.Available)
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
             {
                 // Create and hold a reference to your FirebaseApp,
                 // where app is a Firebase.FirebaseApp property of your application class.
+                firebaseAppRef = FirebaseApp.DefaultInstance;
                 dbRef = FirebaseDatabase.DefaultInstance.RootReference;
-                auth = FirebaseAuth.DefaultInstance;
                 // Set a flag here to indicate whether Firebase is ready to use by your app.
-
-                Debug.Log("Firebase Initialized");
             }
             else
             {
-                Debug.LogErrorFormat("Could not resolve all Firebase dependencies: {0}", dependencyStatus);
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
                 // Firebase Unity SDK is not safe to use here.
             }
         });
+
+
     }
-
-    public async void SaveUser(UserDataModel userData)
-    {
-        string dataInJSON = JsonUtility.ToJson(userData);
-        Debug.Log("Save User :: JSON DATA :: "+dataInJSON);
-        await dbRef.Child("Users").Child(userData.UserId).SetRawJsonValueAsync(dataInJSON);
-    }
-
-
 
 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
-
-    void Awake()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
+        
     }
 
 }
