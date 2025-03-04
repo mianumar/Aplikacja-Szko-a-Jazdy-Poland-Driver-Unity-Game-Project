6f81a4fb7b374df3f488b85d21fb45da00d0a596
Muhammad Umar <50618335+mianumar@users.noreply.github.com>
2025-03-04 16:54:19 +0500

    ScriptAdded to AfterExamAnswersView

--- Assets/Scripts/UI/AfterExamAnswersView.cs
+++ Assets/Scripts/UI/AfterExamAnswersView.cs
@@ -1,10 +1,37 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class SummaryScreenView : MonoBehaviour
+public class AfterExamAnswersView : MonoBehaviour
 {
+    [SerializeField] private Button buttonClose;
+    
+
+
     public void RenderView()
     {
+        AddListeners();
         gameObject.SetActive(true);
+    }
+
+    private void AddListeners()
+    {
+        buttonClose.onClick.RemoveAllListeners();
+        buttonClose.onClick.AddListener(OnCloseButtonClicked);
+
+    }
+
+    private void OnStartButtonClicked()
+    {
+        UIHandler.Instance.qaPanelView.RenderView();
+        this.Disable();
+    }
+
+    private void OnCloseButtonClicked()
+    {
+        UIHandler.Instance.activitySelectionView.RenderView();
+        this.Disable();
     }
 
     public void Disable()
